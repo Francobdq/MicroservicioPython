8293cb34f87ddf8e347c07b9936869ab818333f5
francobdq <fbdq.dev@gmail.com>
2021-03-29 14:42:25 -0300

    Si existe db no se ejecuta el script .sql

--- bdd.py
+++ bdd.py
@@ -4,6 +4,7 @@ import logging
 
 from sqlite3 import Error
 
+nombreSQLSCRIPT = "bdd_sql.sql"
 database = os.getcwd() + "\\sqlite_saludos.db"
 
 def crear_conexion(db_file):
@@ -47,18 +48,25 @@ def obtener_dato(conn, dato):
     return usuarios
 
 # Se inicializa la base de datos
-def inicializar_db():            
+def inicializar_db():          
+
+    # Se comprueba la existencia de la db
+    existeDB = os.path.isfile(database) 
     # se intenta crear la conexión      
     conn = crear_conexion(database) 
-
     if conn is not None:     
         try:
-            logging.debug("Creando tabla en la DB...")
-            # se abre el archivo .sql con las intrucciones para generar la base de datos
-            sql_file = open("bdd_sql.sql")
-            sql_as_string = sql_file.read()
-            # luego se executa en forma de script estas instrucciones (creando así la base de datos) 
-            conn.executescript(sql_as_string)
+            # Si no existe se crean las tablas
+            if(not existeDB):
+                logging.debug("Creando tablas en la db...")
+                # se abre el archivo .sql con las intrucciones para generar la base de datos
+                sql_file = open(nombreSQLSCRIPT)
+                # se lee el archivo
+                sql_as_string = sql_file.read()
+                # luego se executa en forma de script estas instrucciones (creando así la base de datos) 
+                conn.executescript(sql_as_string)
+            else:
+                logging.debug("Las tablas no necesitan ser creadas. Ya lo están.")
         except Error as e:
             logging.error(e)
     else:
